7d3d0a00d877e596f4b1504fd48afb432e08fed3
sandravah12345 <sandravahtel4@gmail.com>
2022-08-03 22:22:51 +0200

    Add files via upload

---
+++ includes/footer.php
@@ -0,0 +1,62 @@
+<footer>
+        <div class="row primary">
+            <div class="column about">
+          
+            <h2 >Nous contacter</h2>
+                <p >Ecrivez nous : help@MyEcoMontre.fr</p>
+               <p> Joignez-nous : 09.23.54.62.85</p>
+          </div>
+          
+          <div class="column links">
+          <h2>Services</h2>
+          
+           <ul class="footer-column">
+          
+            <li class="footer-list">
+             <a href="./faq2.php">F.A.Q</a>
+            </li>
+            <li class="footer-list">
+             <a href="./livraison.php">Livraison</a>
+            </li>
+            <li class="footer-list">
+             <a href="./service_client.php">Service client</a>
+            </li>
+            <li class="footer-list">
+             <a href="./sav.php">Service après-vente</a>
+            </li>
+           </ul>
+          
+          </div>
+          
+          
+          <div class="column links">
+            <h2>Informations</h2>
+             <ul class="footer-column">
+              <li class="footer-list">
+               <a href="./cgv.php">CGV</a>
+              </li>
+              <li class="footer-list">
+               <a href="./politique.php">Vie privée</a>
+              </li>
+              <li class="footer-list">
+              <a href="notice.php">Notice</a>
+              </li>
+            </ul>
+          </div>
+          
+          <div class="column subscribe">
+           <h2>Newsletter</h2>
+            <div>
+             <input type="email" placeholder="Adresse mail" />
+             <button>Envoyer</button>
+            </div>
+          
+          </div>
+          
+          </div>
+          
+          <div class="copyright">
+             <p>Copyright &copy; 2022 MyEcoMontre</p>
+          </div>
+          </footer>
+</section>
---
+++ includes/header.php
@@ -0,0 +1,25 @@
+<header>
+<section>
+    <nav class="myeco-navbar">
+        <div class="myeco-nav">
+          <a href="./index.php"><img src="./images/logomyeco.png" class="myeco-logo" alt="">
+        <div class="myeco-nav-items">
+            <div class="myeco-search">
+                <input type="text" class="myeco-search-box" placeholder="Recherche ..">
+                <button class="myeco-search-btn">Recherche</button>
+            </div>
+            <a href="./login.html"><img src="./images/user.png" alt=""></a>
+            <a href="./checkout.html"><img src="./images/cart.png" alt=""></a>
+            <a href="./checkout.html"><img src="./images/heart.jpg" alt=""></a>
+        </div>
+        </div>
+        <ul class="myeco-links-container">
+            <li class="myeco-link-item"><a href="./homme.php" class="myeco-link">Hommes</a></li>
+            <li class="myeco-link-item"><a href="./femme.php" class="myeco-link">Femmes</a></li>
+            <li class="myeco-link-item"><a href="./junior.php" class="myeco-link">Juniors</a></li>
+            <li class="myeco-link-item"><a href="./reparation.php" class="myeco-link">Services</a></li>
+            <li class="myeco-link-item"><a href="./histoire.php" class="myeco-link">Les origines de MyEco</a></li>
+        </ul>
+    </nav>
+</section>
+</header>
